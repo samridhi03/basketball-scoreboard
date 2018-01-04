8132938f422dda75fef463064752f75e0fdf4ed7
samridhi03 <pargalsamridhi3@gmail.com>
2018-01-04 18:37:57 +0530

    Add files via upload
    user input android application

---
+++ basketballscoreboard/MainActivity.java
@@ -0,0 +1,127 @@
+package com.pargalsamridhi.basketballscoreboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int timer=0;
+    int scoreTeamA=0;
+    int scoreTeamB=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    displayForTeamA(scoreTeamA);
+    displayForTeamB(scoreTeamB);
+    }
+
+
+
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+  int foulA=0;
+    int foulB=0;
+
+
+    public void foulA(View v){
+        scoreTeamA-=2;
+        foulA++;
+        displayForTeamA(scoreTeamA);
+        displayForTeamAFOUL(foulA);
+    }
+
+
+    public void foulB(View v){
+        scoreTeamB-=2;
+        foulB++;
+        displayForTeamB(scoreTeamB);
+        displayForTeamBFOUL(foulB);
+    }
+
+
+    public void displayForTeamAFOUL(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.foul_a_value);
+        scoreView.setText(String.valueOf(score));
+    }
+    public void displayForTeamBFOUL(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.foul_b_value);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+
+}
